92e67c66566c59f4113d6019183c03b5519ba79b
Muhammad Azri <lenovorosak@gmail.com>
2021-10-20 00:25:34 +0800

    Feature: Fix Readme and randomize color to svg background

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -14,15 +14,21 @@ contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
-  // So, we make a baseSvg variable here that all our NFTs can use.
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  // We split the SVG at the part where it asks for the background color.
+  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
   // I create three arrays, each with their own theme of random words.
   // Pick some random funny words, names of anime characters, foods you like, whatever! 
   string[] firstWords = ['Affin', 'Alliance',  'AmBank', 'CIMB', 'HongLeong', 'Maybank', 'RHB', 'BankOfChina', 'JPMorgan',  'MUFG', 'BPCE', 'CitiBank', 'RoyalBankOfCanada', 'HSBC' , 'StandardChartered'];
   string[] secondWords = ['AttackOnTitan', 'FMA', 'HunterXHunter', 'Naruto', 'OnePiece', 'DemonSlayer', 'EyeShield21','FistOfTheNorthStar', 'TokyoGhoul', 'FairyTail', 'RaveMaster', 'OnePunchMan', 'Slamdunk', 'Bleach', 'Dragonball'];
   string[] thirdWords = ['Lenovo','HP','Dell','Apple','Acer','Asus','Google', 'Razer','Microsoft','MSI','Apple','RealMe','Gigabyte','Walmart' , 'Zeuslap'];
+
+  // Get fancy with it! Declare a bunch of colors.
+  string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
+
+
+  event NewEpicNFTMinted(address sender, uint256 tokenId);
 
   constructor() ERC721 ("SquareNFT", "SQUARE") {
     console.log('This is my NFT contract. Woah!');
@@ -49,6 +55,12 @@ contract MyEpicNFT is ERC721URIStorage {
     return thirdWords[rand];
   }
 
+  function pickRandomColor(uint256 tokenId)public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    rand = rand % colors.length;
+    return colors[rand];
+  }
+
   function random(string memory input) internal pure returns (uint256) {
     return uint256(keccak256(abi.encodePacked(input)));
   }
@@ -62,8 +74,10 @@ contract MyEpicNFT is ERC721URIStorage {
     string memory third = pickRandomThirdWord(newItemId);
     string memory combinedWord = string(abi.encodePacked(first, second, third));
 
+
     // I concatenate it all together, and then close the <text> and <svg> tags.
-    string memory finalSvg = string(abi.encodePacked(baseSvg,combinedWord, "</text></svg>"));
+    string memory randomColor = pickRandomColor(newItemId);
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
     // Get all the JSON metadata in place and base64 encode it.
     string memory json = Base64.encode(
@@ -97,5 +111,7 @@ contract MyEpicNFT is ERC721URIStorage {
     
     _tokenIds.increment();
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+    emit NewEpicNFTMinted(msg.sender, newItemId);
   }
 }
